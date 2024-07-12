094716534d70b4ba90ae99f6232269eb27f4af9e
Vadim Kantorov <vadimkantorov@gmail.com>
2024-07-12 16:07:07 +0200

    ...

--- zrxivrender.py
+++ zrxivrender.py
@@ -4,17 +4,19 @@ import nanojekyll
 
 data_dir = 'data/old_documents'
 config_yml = '_config.yml'
-layout_html = '_layouts/default.html'
+layouts_dir = '_layouts/'
 codegen_py = 'nanojekyllcodegen.py'
 test_html = 'test.html'
 
-config = nanojekyll.yaml_loads(open(config_yml).read())
+config = nanojekyll.NanoJekyllContext.yaml_loads(open(config_yml).read())
 
-templates_all = {os.path.splitext(os.path.basename(layout_html))[0] : nanojekyll.NanoJekyllTemplate.read_template(layout_html)}
+#["bibtex.html",  "default.html",  "json.html",  "txt.html",  "xml.html"]
+
+templates_all = {os.path.splitext(layout_html)[0] : nanojekyll.NanoJekyllContext.read_template(os.path.join(layouts_dir, layout_html)) for layout_html in ['default.html'] }#os.listdir(layouts_dir)}
 
 docs = {basename : json.load(open(os.path.join(data_dir, basename))) for basename in os.listdir(data_dir) if basename.endswith('.json')}
     
-cls, python_source = nanojekyll.NanoJekyllTemplate.codegen({k : v[1] for k, v in templates_all.items()}, global_variables = ['site', 'page'])
+cls, python_source = nanojekyll.NanoJekyllContext.codegen({k : v[1] for k, v in templates_all.items()}, global_variables = ['site', 'page'])
 with open(codegen_py, 'w') as f:
     f.write(python_source)
 #cls = __import__(os.path.splitext(codegen_py)[0]).NanoJekyllContext
@@ -24,7 +26,7 @@ ctx = dict(site = config)
 ctx['site']['data'] = dict(documents = list(docs.items()))
 ctx['site']['pages'] = []
 
-content = cls(ctx).render(os.path.splitext(os.path.basename(layout_html))[0])
+content = cls(ctx).render('default')
 
 print(content, file = open(test_html, 'w'))
 print(test_html)
